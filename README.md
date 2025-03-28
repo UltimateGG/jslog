b1cbfe8380ed8d5eafb34656fe8a3b34536b8a58
UltimateGG <clubblock1@gmail.com>
2025-03-28 13:57:56 -0500

    v0.0.6 Logging QOL, timestamp change

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # JS Log
+
 My logging library util for JavaScript/TypeScript projects.
 
 Creates a `logs/` folder in the root of your project and generates `MM-DD-YYYY.log` files. Prints formatted/expanded objects aswell.
 
+Log _files_ will have content, example:
 
-Log *files* will have content, example:
 ```
-[12/29/2023 at 16:01:59 | D: 0s] [INFO]: test 
+[12/29/2023 4:01:59 PM | D: 0s] [INFO]: test
 ```
 
 Where "D: 0s" is the time since the last message. I love having this little feature in my logs because I like to quickly see how far apart messages are (Ex for purchases, errors, etc.).
@@ -14,6 +15,7 @@ Where "D: 0s" is the time since the last message. I love having this little feat
 The library also uses the native `console.log` (Or warn/error) function to print, so formatted objects will show. These messages do not contain the timestamp or diff for cleanliness sake.
 
 # Usage
+
 Just import the logInfo/logWarn/logError function and call it with your string/object. Just like the console.log function it accepts multiple arguments.
 
 ```typescript
@@ -21,5 +23,3 @@ import { logInfo } from '@ultimategg/logging';
 
 logInfo('Hello', { a: true });
 ```
-
-The reason I used function names and not params (Ex LogType.WARN) is just for developer experience and being brief.
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import fs from 'fs';
-
+import ms from 'ms';
 
 enum LogType {
   INFO = 'INFO',
   WARN = 'WARN',
-  ERROR = 'ERROR/FATAL',
+  ERROR = 'ERROR/FATAL'
 }
 
-let currentLogFile = `./logs/${getDate().replace(/\//g, '-')}.log`;
+let currentLogFile = `./logs/${new Date().toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }).replace(/\//g, '-')}.log`;
 let lastLogTime = Date.now();
-
 
 if (!fs.existsSync('./logs')) fs.mkdirSync('./logs');
 
@@ -22,53 +21,37 @@ const logInfo = async (...args: any[]) => pipeLog(LogType.INFO, ...args);
 const logWarn = async (...args: any[]) => pipeLog(LogType.WARN, ...args);
 const logError = async (...args: any[]) => pipeLog(LogType.ERROR, ...args);
 
-
 const pipeLog = (prefix: LogType, ...args: any[]) => {
-  const diff = Math.round((Date.now() - (lastLogTime)) / 1000);
-  const diffString = `D: ${diff > 60 ? Math.round(diff / 60)+'m' : diff+'s'}`;
+  const diffString = `D: ${ms(Math.round(Date.now() - lastLogTime))}`;
 
   console[getFunc(prefix)](`[${prefix}]:`, ...args);
   fs.appendFileSync(currentLogFile, `[${getTimestamp()} | ${diffString}] [${prefix}]: ${args.map(formatArg).join(' ')}\n`);
   lastLogTime = Date.now();
-}
+};
 
 const getFunc = (type: LogType) => {
   switch (type) {
-    case LogType.INFO: return 'log';
-    case LogType.WARN: return 'warn';
-    case LogType.ERROR: return 'error';
-    default: return 'log';
+    case LogType.INFO:
+      return 'log';
+    case LogType.WARN:
+      return 'warn';
+    case LogType.ERROR:
+      return 'error';
+    default:
+      return 'log';
   }
-}
+};
 
 const formatArg = (arg: any) => {
   if (arg instanceof Error) return arg.stack;
   if (typeof arg === 'object') return '\n' + JSON.stringify(arg, null, 2);
   return arg;
-}
+};
 
-const getTimestamp = () => {
-  const date = new Date();
-  const time = String(date.getHours()).padStart(2, '0') + ':' + String(date.getMinutes()).padStart(2, '0') + ':' + String(date.getSeconds()).padStart(2, '0');
-
-  return `${getDate()} at ${time}`;
-}
+const getTimestamp = () => `${getDate()} ${new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: '2-digit', second: '2-digit', hour12: true })}`;
 
 function getDate() {
-  const today = new Date();
-  const dd = String(today.getDate()).padStart(2, '0');
-  const mm = String(today.getMonth() + 1).padStart(2, '0');
-  const yyyy = today.getFullYear();
-
-  return mm + '/' + dd + '/' + yyyy;
+  return new Date().toLocaleDateString('en-US');
 }
 
-
-export {
-  LogType,
-  logInfo,
-  logWarn,
-  logError,
-  getTimestamp,
-  getDate,
-};
+export { LogType, logInfo, logWarn, logError, getTimestamp, getDate };
